5df0150cd66e40d2183749e66879082c109cef6d
Timothy Cederdahl <timothycederdahl@yahoo.com>
2016-12-09 23:54:36 -0500

    Initial push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            //Here MyDatabaseEntities is our entity datacontext (see Step 4)
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Events.OrderBy(a => a.StartAt).ToList();
+                return new JsonResult { Data = v, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event evt)
+        {
+            bool status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                if (evt.EndAt != null &&  evt.StartAt.TimeOfDay == new TimeSpan(0,0,0) && 
+                    evt.EndAt.Value.TimeOfDay == new TimeSpan(0,0,0))
+                {
+                    evt.IsFullDay = true;
+                }
+                else
+                {
+                    evt.IsFullDay = true;
+                }
+
+                if(evt.EventID > 0)
+                {
+                    var v = dc.Events.Where(a => a.EventID.Equals(evt.EventID)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Title = evt.Title;
+                        v.Description = evt.Description;
+                        v.StartAt = evt.StartAt;
+                        v.EndAt = evt.EndAt;
+                        v.IsFullDay = evt.IsFullDay;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(evt);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            bool status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Events.Where(a => a.EventID.Equals(eventID)).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
